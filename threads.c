d1e5d37225de1b89f21c5c69478939eeca832d69
niall-healy <48368862+niall-healy@users.noreply.github.com>
2019-04-12 12:12:04 -0400

    Create threads.c

---
+++ threads.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 4
+
+void reader();
+void character();
+void toUpper();
+void writer();
+
+int main(void)
+{
+  
+}
